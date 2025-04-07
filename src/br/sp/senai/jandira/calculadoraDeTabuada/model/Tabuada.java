cd60d91504102bcddd80fd98693e7b0677c9302d
WESLEI SANTOS BEZERRA <25133277@cfp127aluno.local>
2025-04-07 11:26:07 -0300

    Projeto atualizado

--- src/br/sp/senai/jandira/calculadoraDeTabuada/model/Tabuada.java
+++ src/br/sp/senai/jandira/calculadoraDeTabuada/model/Tabuada.java
@@ -5,39 +5,54 @@ public class Tabuada {
 	private double minimoMultiplicador;
 	private double maximoMultiplicador;
 	private double multiplicando;
+	
 	public double getMinimoMultiplicador() {
 		return minimoMultiplicador;
 	}
+	
 	public void setMinimoMultiplicador(double minimoMultiplicador) {
 		this.minimoMultiplicador = minimoMultiplicador;
 	}
+	
 	public double getMaximoMultiplicador() {
 		return maximoMultiplicador;
 	}
+	
 	public void setMaximoMultiplicador(double maximoMultiplicador) {
 		this.maximoMultiplicador = maximoMultiplicador;
 	}
+	
 	public double getMultiplicando() {
 		return multiplicando;
 	}
+	
 	public void setMultiplicando(double multiplicando) {
 		this.multiplicando = multiplicando;
 	}
 	
-	public void calcularTabuada() {
+	public String [] calcularTabuada() {
 		if (minimoMultiplicador>maximoMultiplicador) {
 			double temp = minimoMultiplicador;
 			minimoMultiplicador = maximoMultiplicador;
 			maximoMultiplicador = minimoMultiplicador;
 		}
 		
+		// Criar um vetor de String que armazenará cada linha da tabuada
+		int tamanho = (int) (maximoMultiplicador - minimoMultiplicador + 1);
+		String resultado[] = new String [tamanho];
+		
+		int contador = 0;
 		while (minimoMultiplicador <= maximoMultiplicador) {
 			double produto = multiplicando * minimoMultiplicador;
-			System.out.printf("%s X %s = %s", multiplicando, minimoMultiplicador, produto);
+			String linha = multiplicando + " X " + minimoMultiplicador + " = " + produto;
+			resultado[contador] = linha;
+			System.out.println(linha);
 			System.out.println();
 			minimoMultiplicador++;
+			contador++;
 		}
 		System.out.println("FIM!");
+		return resultado;
 	}
 
 }
--- src/gui/TelaTabuada.java
+++ src/gui/TelaTabuada.java
@@ -1,50 +1,120 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextField;
 
+import br.sp.senai.jandira.calculadoraDeTabuada.model.Tabuada;
+
 public class TelaTabuada {
+
+	private JLabel lblMultiplicando;
+	private JTextField txtMultiplicando;
+	private JLabel lblMinMultiplicador;
+	private JTextField txtMinMultiplicador;
+	private JLabel lblMaxMultiplicador;
+	private JTextField txtMaxMultiplicador;
+	private JButton buttonCalcular;
+	private JButton buttonLimpar;
+	private JList listTabuada;
+	private String resultado[];
 	
 	public void criarTela() {
 		
 		JFrame tela = new JFrame();
-		tela.setSize(500, 700);
+		tela.setSize(265, 550);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		tela.setTitle("Tabuada");
 		tela.setLocationRelativeTo(null);
+		// para abrir a programação no meio da tela 
 		tela.setLayout(null);
+		tela.setResizable(false);
+		// para permitit ou não redimensionar a tela
+		
+	
 		
 		// Multiplicando
-		JLabel lblMultiplicando = new JLabel();
+		lblMultiplicando = new JLabel();
 		lblMultiplicando.setText("Multiplicando:");
-		lblMultiplicando.setBounds(20, 10, 120, 30);
+		lblMultiplicando.setBounds(20, 10, 100, 20);
 		
-		
-		JTextField txtMultiplicando = new JTextField();
-		txtMultiplicando.setBounds(140, 10, 50, 30);
-		
-		
-		JButton buttonCalcular = new JButton();
-		buttonCalcular.setText("Calcular");
+		txtMultiplicando = new JTextField();
+		txtMultiplicando.setBounds(180, 10, 50, 20);
 		
 		// Mínimo Multiplicador
-		JLabel lblMinMultiplicador = new JLabel();
+		lblMinMultiplicador = new JLabel();
 		lblMinMultiplicador.setText("Mín. Multiplicador: ");
-		lblMinMultiplicador.setBounds(20, 50, 150, 30);
+		lblMinMultiplicador.setBounds(20, 50, 150, 20);
 		
-		JTextField txtMinMultiplicador = new JTextField();
-		txtMinMultiplicador.setBounds(140, 50, 50, 30);
+		txtMinMultiplicador = new JTextField();
+		txtMinMultiplicador.setBounds(180, 50, 50, 20);
 		
+		// Máximo multiplicador
+		lblMaxMultiplicador = new JLabel();
+		lblMaxMultiplicador.setText("Máx. Multiplicador: ");
+		lblMaxMultiplicador.setBounds(20, 90, 150, 20);
+		
+		txtMaxMultiplicador = new JTextField();
+		txtMaxMultiplicador.setBounds(180, 90, 50, 20);
+		
+		// botão calcular
+		buttonCalcular = new JButton();
+		buttonCalcular.setText("Calcular");
+		buttonCalcular.setBounds(20, 130, 100, 30);
+		
+		// botão limpar
+		buttonLimpar = new JButton();
+		buttonLimpar.setText("Limpar");
+		buttonLimpar.setBounds(130, 130, 100, 30);
+		
+		// Lista para exibir a tabuada
+		listTabuada = new JList();
+		listTabuada.setBounds(20, 180, 210, 300);
+		
+		
+		
+		// Adicionar componentes ao painel de conteúdo
 		tela.getContentPane().add(lblMultiplicando);
 		tela.getContentPane().add(lblMinMultiplicador);
+		tela.getContentPane().add(lblMaxMultiplicador);
 		tela.getContentPane().add(txtMinMultiplicador);
 		tela.getContentPane().add(txtMultiplicando);
-		//tela.getContentPane().add(buttonCalcular);
+		tela.getContentPane().add(txtMaxMultiplicador);
+		tela.getContentPane().add(buttonCalcular);
+		tela.getContentPane().add(buttonLimpar);
+		tela.getContentPane().add(listTabuada);
 		
+
+		// Configurar os ouvintes (listeners) dos botões
+		buttonCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String multiplicando = txtMultiplicando.getText();
+				String minMultiplicador = txtMinMultiplicador.getText();
+				String maxMultiplicador = txtMaxMultiplicador.getText();
+				
+				// Casting -> Conversão de um tipo para outro
+				double multiplicandoDouble = Double.parseDouble(multiplicando);
+				double minMultiplicadorDouble = Double.parseDouble(minMultiplicador);
+				double maxMultiplicadorDouble = Double.parseDouble(maxMultiplicador);
+				
+				Tabuada tabuada = new Tabuada();
+				tabuada.setMultiplicando(multiplicandoDouble);
+				tabuada.setMinimoMultiplicador(minMultiplicadorDouble);
+				tabuada.setMaximoMultiplicador(maxMultiplicadorDouble);
+				resultado = tabuada.calcularTabuada();
+			
+			}
+		});
 		
+		// Tornar a tela visível ao usuário
 		tela.setVisible(true);
 		
 	}
